756cc69a7e0bc41012fd33d07511ac53bcd50244
Doug Blanding <dblanding@gmail.com>
2026-05-15 06:04:03 -0400

    Remove desktop_code to become stand-alone repo (pose-graph-SLAM)

--- desktop_code/deploy/stop_scan_mtr.py
+++
@@ -1,8 +0,0 @@
-from pyinfra.operations import server
-
-server.shell(
-    name="Stop Scan Motor",
-    commands=["sudo systemctl stop run_scan_mtr.service"],
-    _retries=2,
-    _retry_delay=10,  # 10 second delay between retries
-)
